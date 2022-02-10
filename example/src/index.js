e4a7a497adbc72b64e8772e7659f11727c164cc6
Maulik Patel <93574359+maulikpatel93@users.noreply.github.com>
2022-02-10 12:11:01 +0530

    32

--- example/src/index.js
+++
@@ -1,9 +0,0 @@
-// These must be the first lines in src/index.js
-import "react-app-polyfill/ie11";
-import "react-app-polyfill/stable";
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.scss";
-import App from "./App";
-
-ReactDOM.render(<App />, document.getElementById("root"));
